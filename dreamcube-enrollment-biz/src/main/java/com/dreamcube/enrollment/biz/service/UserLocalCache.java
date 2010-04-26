240ae25df6ae0659c4f4ddfc81c1b24acb622f94
peigen123 <peigen123@c895bc7b-d83b-6f03-f878-0cac0eafc000>
2010-04-26 15:40:32 +0000

    用户缓存也加上了，但是还未测试，考虑就不提共DAO查库，或者根据配置确定走DB还是cache 下一步加强cache-mongodb效率和mysql的参数配置

---
+++ dreamcube-enrollment-biz/src/main/java/com/dreamcube/enrollment/biz/service/UserLocalCache.java
@@ -0,0 +1,29 @@
+package com.dreamcube.enrollment.biz.service;
+
+import com.dreamcube.core.common.service.LocalCache;
+
+/**
+ *                       
+ * Filename: UserLocalCache.java
+ *
+ * Description: 
+ *
+ * Version: 0.1
+ *
+ * Author: peigen
+ *
+ * Email: peigen123@gmail.com
+ *
+ * Blog: http://peigen.info
+ *
+ *       
+ * History:<br>
+ *<li>Author: peigen</li>
+ *<li>Date: 2010-4-26</li>
+ *<li>Version: 0.1</li>
+ *<li>Content: create</li>
+ *
+ */
+public interface UserLocalCache extends LocalCache {
+
+}
