505ff09fbd0bee61f04c10bc677e97b2d148f12d
hao.holly <hao.holly@c895bc7b-d83b-6f03-f878-0cac0eafc000>
2010-06-08 14:30:58 +0000

    多模块内部渲染机制---不过还需要修改，因为 多模块渲染的时候需要将内部渲染模块的 css，layout以及其他内容js等通通去掉。。。这个有点难搞其实

---
+++ dreamcube-core-common/src/main/java/com/dreamcube/framework/components/BeanToolManager.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package com.dreamcube.framework.components;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * BeanToolManager 关于beantool的管理类
+ * 
+ * @author holly May 26, 2010
+ * 
+ */
+public class BeanToolManager implements ApplicationContextAware,
+		InitializingBean {
+	private Map<String, BeanTool> beanTools = new HashMap<String, BeanTool>();
+
+	private ArrayList<String> beanToolsList = new ArrayList<String>();
+
+	private ApplicationContext applicationContext;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.context.ApplicationContextAware#setApplicationContext
+	 * (org.springframework.context.ApplicationContext)
+	 */
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+
+	public ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+
+	public ArrayList<String> getBeanToolsList() {
+		return beanToolsList;
+	}
+
+	public void setBeanToolsList(ArrayList<String> beanToolsList) {
+		this.beanToolsList = beanToolsList;
+	}
+	
+	public Map<String, BeanTool> getBeanTools() {
+		return beanTools;
+	}
+
+	public void setBeanTools(Map<String, BeanTool> beanTools) {
+		this.beanTools = beanTools;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.springframework.beans.factory.InitializingBean#afterPropertiesSet()
+	 */
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		Iterator<String> iterator = beanToolsList.iterator();
+		for (; iterator.hasNext();) {
+			String tool = iterator.next();
+			Class toolClass = Class.forName(tool);
+			BeanTool beanTool = (BeanTool) toolClass.newInstance();
+			this.beanTools.put(beanTool.getToolKey(), beanTool);
+		}
+	}
+
+
+	
+	
+
+}
---
+++ dreamcube-core-common/src/main/java/com/dreamcube/framework/viewresolver/DreamCubeVelocityViewResolver.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.dreamcube.framework.viewresolver;
+
+import org.springframework.web.servlet.view.AbstractUrlBasedView;
+import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
+
+import com.dreamcube.framework.components.BeanToolManager;
+import com.dreamcube.framework.views.DreamCubeVelocityView;
+
+/**
+ * @author holly May 26, 2010
+ *
+ */
+public class DreamCubeVelocityViewResolver extends VelocityLayoutViewResolver {
+	private BeanToolManager beanToolManager;
+	
+	/**
+	 * Requires DreamCubeVelocityView.
+	 * @see DreamCubeVelocityView
+	 */
+	@Override
+	protected Class requiredViewClass() {
+		return DreamCubeVelocityView.class;
+	}
+
+	@Override
+	protected AbstractUrlBasedView buildView(String viewName) throws Exception {
+		DreamCubeVelocityView view = (DreamCubeVelocityView)super.buildView(viewName);
+		view.setBeanToolManager(beanToolManager);
+		return view;
+	}
+
+	public void setBeanToolManager(BeanToolManager beanToolManager) {
+		this.beanToolManager = beanToolManager;
+	}
+	
+	
+	
+	
+}
